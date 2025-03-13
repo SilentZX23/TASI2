6f5b2df3d44b3629051725d3b88d4fcfc7c39572
SilentZX23 <alvarospadinsilva@gmail.com>
2025-03-13 10:25:39 -0300

    Assim é mais facil

---
+++ tiipo2Dado2.js
@@ -0,0 +1,18 @@
+n = 80
+nome = "Jose"
+if(n % 2 === 0)
+{
+    console.log("O número " + n + " é par")
+}
+else
+{
+    console.log("O número " + n + " é impar")
+}
+if(nome === "Jose")
+{
+    console.log("Meu nome é " + nome)
+}
+else
+{
+    console.log("Meu não não é José")
+}
